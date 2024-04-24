c27b2e2425cd66dc1d9dbaabf05e5778bbb6d39d
Moknine Elhadj <93493650+Edjemoknine@users.noreply.github.com>
2024-04-24 20:52:28 +0100

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,10 +9,10 @@ const Skills = () => {
     >
       <div className=" max-w-[1000Px]  mx-auto w-full ">
         <motion.div
-          variants={fadeIn("up", 0.3)} // just update delay or direction
+          variants={fadeIn("up", 0.1)} // just update delay or direction
           initial="hidden"
           whileInView={"show"}
-          viewport={{ once: true, amount: 0.7 }}
+          viewport={{ once: true, amount: 0.5 }}
         >
           <div className="dark:text-white w-full text-4xl font-bold inline border-b-4 border-teal-500">
             Skills
@@ -24,10 +24,10 @@ const Skills = () => {
         <div className="grid md:grid-cols-6 sm:grid-cols-3 grid-cols-2  gap-4 mt-3">
           {skills.map((skill, index) => (
             <motion.div
-              variants={fadeIn("up", 0.2 * index)} // just update delay or direction
+              variants={fadeIn("up", 0.05 * index)} // just update delay or direction
               initial="hidden"
               whileInView={"show"}
-              viewport={{ once: true, amount: 0.7 }}
+              viewport={{ once: true, amount: 0.5 }}
               key={index}
               className="flex flex-col dark:text-white min-h-[100px] rounded shadow-md justify-around hover:shadow-xl cursor-pointer items-center  mt-2"
             >

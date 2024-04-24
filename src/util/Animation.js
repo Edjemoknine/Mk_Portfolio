d065938c7d967cb2b8088bc57ef8cde397caa25a
Moknine Elhadj <93493650+Edjemoknine@users.noreply.github.com>
2024-04-24 20:51:06 +0100

    Update Animation.js

--- src/util/Animation.js
+++ src/util/Animation.js
@@ -11,7 +11,7 @@ export const fadeIn = (direction, delay) => {
       opacity: 1,
       transition: {
         type: "tween",
-        duration: 1.2,
+        duration: 1,
         delay: delay,
         ease: [0.25, 0.25, 0.25, 0.75],
       },
